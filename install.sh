ce06bd38cf6e02c40c65498277cc11f98e7513fe
Michael Bennett <michaelbennett.dev@gmail.com>
2019-04-22 20:48:53 -0700

    Update install script with comments/YCM.

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-cp vimrc ~/.vim
+# Make the folders we'll be using.
 mkdir -p ~/.vim/.more
 mkdir -p ~/.dotfiles
+
+# Install generic configuration. (Aliases mostly)
 cp git_aliases ~/.dotfiles
 cp custom_profile ~/.dotfiles
 cp more_aliases ~/.dotfiles
+
+# Install Vim configuration.
+cp vimrc ~/.vim
 cp vim_more ~/.vim/.more
 cp vim_python ~/.vim/.more
 cp vim_extensions ~/.vim/.more
+
+# Install YouCompleteMe generic script.
+cp ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+
 if [ -f ~/.profile ]; then
     echo ". ~/.dotfiles/.custom_profile" >> ~/.profile
 elif [ -f ~/.bash_profile ]; then
@@ -18,4 +27,4 @@ elif [ -f ~/.bashrc ]; then
 else
     echo "Could not find a place to stick the sourcing of ~/.dotfiles/.custom_profile !"
 fi
-# Don't source it - just in case :) 
+
