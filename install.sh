MJJBennett/dotfiles

commit ca3785528aff4fb8764438efdc14403e80b8aec7

Track output files of commit script for future use to ensure concats don't change anything major.

install.sh
@@ -53,19 +53,24 @@ fi
 echo "Installing generic configuration files."
 # Install generic configuration. (Aliases mostly)
 cp git_aliases ~/.dotfiles
+cp git_aliases output-clone/.dotfiles
 cp custom_profile ~/.dotfiles
+cp custom_profile output-clone/.dotfiles
 cp more_aliases ~/.dotfiles
+cp more_aliases output-clone/.dotfiles
 
 echo "Installing Vim configuration files."
 # Install Vim configuration.
 cp vimrc ~/.vim
-cp vim_more ~/.vim/.more
-cp vim_python ~/.vim/.more
-cp vim_extensions ~/.vim/.more
+cp vimrc output-clone/.vim
+cp vim_more output-clone/.vim/.more
+cp vim_python output-clone/.vim/.more
+cp vim_extensions output-clone/.vim/.more
 
 echo "Installing YouCompleteMe handler."
 # Install YouCompleteMe generic script.
 cp ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+cp ycm_extra_conf.py output-clone/.vim/.ycm_extra_conf.py
 
 # Make a marker that this script has been run, so backups aren't made again.
 touch ~/.dotfiles/old/.marker

output-clone/.vim/.more/vim_more
@@ -1,0 +1,28 @@
+" Basically, more vim stuff.
+" Why not?
+
+" au BufNewFile,BufRead *.md :set spell spelllang=en_ca<CR>
+
+nnoremap <Leader>e :!
+nnoremap <Leader>cf :!cat %<CR>
+" This makes s work as no-copy d
+nnoremap s "_d
+" Repeat previous macro
+nnoremap Q @@
+" Opposite of J
+nnoremap <Leader>n i<CR><ESC>
+" Save files faster
+nnoremap <Leader>s :w<CR>
+
+set foldmethod=syntax
+
+function FixQuotes()
+    " This function fixes more than just quotes
+    " However, it is called 'FixQuotes'
+    %s/“/"/ge
+    %s/”/"/ge
+    %s/’/'/ge
+    %s/‘/'/ge
+endfunction
+
+nnoremap <Leader>fq :call FixQuotes()<CR>

concat_compatible.sh
@@ -1,0 +1,7 @@
+#!/bin/bash
+
+# Script to concatenate together a set of files for editors that can take .vimrc files
+# As compatible as possible
+
+cat vimrc > .vimrc.compatible
+cat 

output-clone/.dotfiles/custom_profile
@@ -1,0 +1,6 @@
+. ~/.dotfiles/git_aliases
+. ~/.dotfiles/more_aliases
+
+if [ -f ~/.dotfiles/local_profile ]; then 
+    . ~/.dotfiles/local_profile
+fi

output-clone/.dotfiles/git_aliases
@@ -1,0 +1,49 @@
+# Note: Can get the current branch as follows:
+# current_branch=$(git symbolic-ref --short -q HEAD)
+
+alias s="git status"
+alias l="git log"
+# Pretty log taken from "Becoming a ZSH Power User in 80 Minutes" (most likely not original source)
+alias plog="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches"
+alias b="git branch"
+
+alias fall="git fetch --all"
+alias pull="git pull"
+
+alias dif="git diff ."
+alias gdif="git diff"
+
+alias af="git add ."
+alias afc="git add . && git commit -m"
+function afcp { afc "$1"; git push; }
+function afcpu { afc "$1"; git push -u origin HEAD; }
+function afcpf { afc "$1"; git push -f; }
+alias afa="git add . && git commit --amend"
+
+alias c="git commit -m"
+function gcp { c "$1"; git push; }
+function gcpf { c "$1"; git push -f; }
+
+alias p="git push"
+alias pf="git push -f"
+alias pu="git push -u origin HEAD"
+
+alias sta="git stash"
+alias pop="git stash pop"
+
+alias ecg="echo $BASH_SOURCE"
+alias vg="vim $BASH_SOURCE"
+alias sg=". $BASH_SOURCE"
+alias cg="cat $BASH_SOURCE"
+
+# Interesting github pull request alias, just keeping around for now
+# gpr() {
+#  if [ $? -eq 0 ]; then
+#    github_url=`git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#http://#' -e 's@com:@com/@' -e 's%\.git$%%'`;
+#    branch_name=`git symbolic-ref HEAD 2>/dev/null | cut -d"/" -f 3`;
+#    pr_url=$github_url"/compare/master..."$branch_name
+#    open $pr_url;
+#  else
+#    echo 'failed to open a pull request.';
+#  fi
+#}

output-clone/.dotfiles/more_aliases
@@ -1,0 +1,19 @@
+alias macmake="cmake .. -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ -DCMAKE_C_COMPILER=/usr/local/bin/gcc-8"
+alias macmakecc="macmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+
+alias ccmake="cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+
+alias fcb="ccmake && make all && ctest --verbose"
+
+alias la="ls -la"
+alias usage="du -h -d1"
+alias ze="vim ~/.zshrc"
+
+# This only works with gitify somewhere in the path
+function gch {
+    git clone $(gitify -p "$1") $2; 
+}
+
+# Writing aliases
+alias cw="wc -w *.md"
+alias cwt="cw | egrep '([0-9]+) *total' | egrep -o '[0-9]+'"

output-clone/.vim/.more/vim_extensions
@@ -1,0 +1,63 @@
+" Vundle
+
+set nocompatible
+filetype off
+
+" set the runtime path to include Vundle and initialize
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+
+" let Vundle manage Vundle, required
+Plugin 'VundleVim/Vundle.vim'
+
+Plugin 'Valloric/YouCompleteMe'
+Plugin 'rhysd/vim-clang-format'
+" Emmet - HTML
+Plugin 'mattn/emmet-vim'
+Plugin 'tpope/vim-surround'
+
+" All of your Plugins must be added before the following line
+call vundle#end()            " required
+filetype plugin indent on    " required
+" To ignore plugin indent changes, instead use:
+"filetype plugin on
+"
+" Brief help
+" :PluginList       - lists configured plugins
+" :PluginInstall    - installs plugins; append `!` to update or just :PluginUpdate
+" :PluginSearch foo - searches for foo; append `!` to refresh local cache
+" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal
+"
+" see :h vundle for more details or wiki for FAQ
+" Put your non-Plugin stuff after this line
+
+" YCM Configuration
+let g:ycm_clangd_binary_path = "/usr/local/Cellar/llvm/8.0.0/bin/clangd/"
+let g:ycm_server_keep_logfiles = 1
+let g:ycm_server_log_level = 'debug'
+let g:ycm_global_ycm_extra_conf = "~/.vim/.ycm_extra_conf.py"
+let g:ycm_extra_conf_globlist = ['~/.vim/.ycm_extra_conf.py']
+let g:ycm_confirm_extra_conf = 0
+let g:ycm_autoclose_preview_window_after_completion = 1
+let g:ycm_autoclose_preview_window_after_insertion = 1
+let g:ycm_semantic_triggers = {
+    \ 'C': [ 're!\w{4}' ]
+    \ }
+
+" Clang Format Configuration
+let g:clang_format#style_options = {
+    \ "BreakBeforeBraces" : "Allman",
+    \ "AlwaysBreakTemplateDeclarations" : "true",
+    \ "AccessModifierOffset" : -4,
+    \ "PointerAlignment" : "Left",
+    \ "ColumnLimit" : "99",
+    \ "AllowShortCaseLabelsOnASingleLine" : "true",
+    \ "AllowShortFunctionsOnASingleLine" : "All",
+    \ "AllowShortIfStatementsOnASingleLine" : "true",
+    \ "TabWidth" : 4,
+    \ "UseTab" : "Never",
+    \ "AlignOperands" : "true",
+    \ "AlignTrailingComments" : "true",
+    \ "AlignConsecutiveAssignments" : "true",
+    \ "Standard" : "C++11"}
+

output-clone/.vim/.more/vim_python
@@ -1,0 +1,14 @@
+au BufNewFile,BufRead *.py
+    \ set tabstop=4 |
+    \ set softtabstop=4 |
+    \ set shiftwidth=4 |
+    \ set textwidth=100 |
+    \ set expandtab |
+    \ set foldmethod=indent |
+    \ set autoindent |
+    \ set fileformat=unix
+
+" au BufRead,BufNewFile *.py,,*.cpp,*.h match BadWhitespace /\s\+$/
+
+set encoding=utf-8
+

commit b626badc461f3fb8d75d4a033288245f7c576444

Fix bug in install script introduced in ca3785528a. Add alias for counting words in file.

install.sh
@@ -65,6 +65,9 @@ cp vimrc ~/.vim
 cp vimrc output-clone/.vim
 # Delete vimrc (it's generated in this script anyways)
 rm vimrc
+cp vim/vim_more ~/.vim/.more
+cp vim/vim_python ~/.vim/.more
+cp vim/vim_extensions ~/.vim/.more
 cp vim/vim_more output-clone/.vim/.more
 cp vim/vim_python output-clone/.vim/.more
 cp vim/vim_extensions output-clone/.vim/.more

output-clone/.vim/.more/vim_more
@@ -5,6 +5,7 @@
 
 nnoremap <Leader>e :!
 nnoremap <Leader>cf :!cat %<CR>
+nnoremap <Leader>cw :!wc -w %<CR>
 " This makes s work as no-copy d
 nnoremap s "_d
 " Repeat previous macro

vim/vim_more
@@ -5,6 +5,7 @@
 
 nnoremap <Leader>e :!
 nnoremap <Leader>cf :!cat %<CR>
+nnoremap <Leader>cw :!wc -w %<CR>
 " This makes s work as no-copy d
 nnoremap s "_d
 " Repeat previous macro