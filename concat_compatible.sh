29bf0043608344f2fc2a8fa01a4ce255d6396968
Michael Bennett <michaelbennett.dev@gmail.com>
2019-11-08 20:07:43 -0800

    Small updates.

--- concat_compatible.sh
+++ concat_compatible.sh
@@ -3,6 +3,6 @@
 # Script to concatenate together a set of files for editors that can take .vimrc files
 # As compatible as possible
 
-cat vimrc-src > .vimrc.compatible
-cat vim_more >> .vimrc.compatible
-cat vim_python >> .vimrc.compatible
+cat vim/vimrc-src > .vimrc.compatible
+cat vim/vim_more >> .vimrc.compatible
+cat vim/vim_python >> .vimrc.compatible
