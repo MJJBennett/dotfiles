6570eb011073f599832b3b3ff62dec8694a342b7
Michael Bennett <michaelbennett.dev@gmail.com>
2020-09-29 13:19:46 -0700

    ...add YouCompleteMe post-install step.

--- post-install.sh
+++ post-install.sh
@@ -5,3 +5,6 @@ mkdir -p ~/.vim/plugin
 
 # Setup: vim-autopep8
 cp ~/.vim/bundle/vim-autopep8/ftplugin/python_autopep8.vim ~/.vim/plugin/
+
+# Setup: YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe/ && ./install.py --clang-completer
