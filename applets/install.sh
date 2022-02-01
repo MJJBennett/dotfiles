54d48095ae9fcff46a143a0414c43238f246b61e
Michael Bennett <michaelbennett.dev@gmail.com>
2022-01-31 23:02:04 -0500

    Applets.

---
+++ applets/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+mkdir -p $HOME/programming/install
+cp $SCRIPT_DIR/*.py $HOME/programming/install
+cp $SCRIPT_DIR/editor_config $HOME/.dotfiles/
---
+++ applets/last_edited.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+"""
+last_edited.py
+
+A python script for caching & reading from that cache the most recently edited file.
+"""
+
+# Quickly port some parsing methods
+def consume(a):
+    if len(a) == 0:
+        a = [None]
+    return (a[0], a[1:])
+
+def parsed(args):
+    params = list()
+    norm = list()
+    skip = False
+    while len(args) > 0:
+        arg, args = consume(args)
+        if skip or not arg.startswith('--'):
+            norm.append(arg)
+            continue
+        if arg == '--':
+            skip = True
+            continue
+        params.append(arg)
+    return (params, norm)
+
+def get_paths_path():
+    from pathlib import Path
+    from os.path import join
+    # This should use XDG_CACHE_HOME
+    return join(Path.home(), '.cache/.last-edited')
+
+def get_paths():
+    return [line.strip() for line in open(get_paths_path(), 'r')]
+
+def update_paths(paths):
+    with open(get_paths_path(), 'w') as file:
+        [file.write(line + '\n') for line in paths]
+
+def get_matches(paths, exprs):
+    return [p for p in paths if all([e in p for e in exprs])]
+
+def get_edit_path(args, paths):
+    from os.path import isfile, abspath
+    if len(args) > 0 and isfile(args[0]):
+        # We are editing this file directly.
+        # Update the cache.
+        return abspath(args[0])
+    if len(args) > 0:
+        # We want to edit some file.
+        # Try to find it.
+        return (get_matches(paths, args) or [args[0]])[0]
+    return (paths or [''])[0]
+
+def main(args):
+    params, args = parsed(args) 
+    paths = get_paths()
+
+    # Parameters:
+    # list-matches - List all files that match the criterion.
+    # Exits early.
+    if 'list-matches' in params:
+        print('\n'.join(get_matches(paths, args)))
+        import sys
+        sys.exit(0)
+
+    # This is the file we want to edit.
+    # If the file already exists
+    edit_path = get_edit_path(args, paths)
+    from os.path import isfile
+    if isfile(edit_path):
+        if edit_path in paths:
+            paths.remove(edit_path)
+        paths.insert(0, edit_path)
+        update_paths(paths)
+    print(edit_path)
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
